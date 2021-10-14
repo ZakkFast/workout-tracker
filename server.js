30d8e7c3b27736bcd4ec48dfd5d0c0e80c75397d
Zakk Fast <zakkfastpro@gmail.com>
2021-10-14 15:57:10 -0700

    testing

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-// const connectDB = require('./config/db');
+const connectDB = require('./config/db');
 
 const app = express();
 //Connect Database
-// connectDB();
+connectDB();
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send("API Running"));
+app.get('/', (req, res) => res.send('API Running'));
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
