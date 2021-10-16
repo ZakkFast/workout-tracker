15ca0d99d86f1e5aed1ae1ed87e620f9aa224ec5
Zakk Fast <zakkfastpro@gmail.com>
2021-10-15 22:16:12 -0700

    prettified

--- server.js
+++ server.js
@@ -10,10 +10,10 @@ app.use(express.json());
 
 // app.get('/', (req, res) => res.send('API Running'));
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use(require('./routes/index.js'))
-app.use(require('./routes/api'))
+app.use(require('./routes/index.js'));
+app.use(require('./routes/api'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
