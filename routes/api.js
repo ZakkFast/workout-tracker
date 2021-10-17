da7e87699df092b6e14c972ff7290d6ce7f55083
Zakk Fast <zakkfastpro@gmail.com>
2021-10-16 18:35:05 -0700

    finished routing

--- routes/api.js
+++ routes/api.js
@@ -1,144 +1,69 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
-const mongoose = require('mongoose')
 
 // @ GET
 // @ Route   /api/workouts
 // @ Decs    Getting all workouts from the DB.
-// router.get('/api/workouts', async (req, res) => {
-//   try {
-//     const workout = await workoutDB.Workout.find({});
+router.get('/api/workouts', async (req, res) => {
+  try {
+    const workout = await db.Workout.find({});
 
-//     if (!workout) {
-//       return res.status(400).json({ msg: 'There is no workout data' });
-//     }
-//     console.log(workoutDB.Workout.find())
-//     res.send(workout);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
+    if (!workout) {
+      return res.status(400).json({ msg: 'There is no workout data' });
+    }
 
-// @ GET
-// @ Route   /api/workouts
-// @ Decs    Getting all workouts from the DB.
-router.get('/api/workouts', (req, res) => {
-  db.Workout.find({})
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+    res.json(workout);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @ POST
 // @ Route   /api/workouts
 // @ Decs    Create new workout
-// router.post('/api/workouts', async ({ body }, res) => {
-//   try {
-//     const workout = await workoutDB.Workout.create(body);
+router.post('/api/workouts', async (req, res) => {
+  try {
+    const workout = await db.Workout.create(req.body);
 
-//     res.json(workout);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
-
-// @ POST
-// @ Route   /api/workouts
-// @ Decs    Create new workout
-router.post('/api/workouts', (req, res) => {
-  db.Workout.create(req.body)
-    .then((data) => {
-      res.json(data);
-      console.log(req.body)
-    })
-    .catch((err) => {
-      res.send(err);
-    });
+    res.json(workout);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @ PUT
 // @ Route   /api/workouts/:id
 // @ Decs    Updated workout
-// router.put('/api/workouts/:id', async ({ body, params }, res) => {
-//   try {
-//     const workout = await workoutDB.Workout.findByIdAndUpdate(
-//       params.id,
-//       { $push: { exercised: body } },
-//       { new: true }
-//     );
+router.put('/api/workouts/:id', async (req, res) => {
+  try {
+    const workout = await db.Workout.findOneAndUpdate(
+      { _id: req.params.id },
+      { $push: { exercises: req.body } },
+      { new: true }
+    );
 
-//     res.json(workout);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
-
-// @ PUT
-// @ Route   /api/workouts/:id
-// @ Decs    Updated workout
-router.put('/api/workouts/:id', (req, res) => {
-  db.Workout.findOneAndUpdate(
-    { _id: req.params.id },
-    { $push: { exercises: req.body } },
-    { new: true }
-  )
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+    res.json(workout);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
-// @ DELETE
-// @ Route   /api/workouts/
-// @ decs    Delete workout by id
-// router.delete('/api/workouts', async ({ body }, res) => {
-//   try {
-//     const workout = await workoutDB.Workout.findByIdAndRemove(body.id);
-
-//     res.json(workout);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
-
 // @ GET
 // @ Route   /api/workouts/range
 // @ decs    Get workout by range
-// router.get('/api/workouts/range', async ({ body }, res) => {
-//   try {
-//     const workout = await workoutDB.Workout
-//     .find({})
-//     .limit(10);
+router.get('/api/workouts/range', async (req, res) => {
+  try {
+    const workout = await db.Workout.find({}).limit(10);
 
-//     res.json(workout);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
-
-// @ GET
-// @ Route   /api/workouts/range
-// @ decs    Get workout by range
-router.get('/api/workouts/range', (req, res) => {
-  db.Workout.find({})
-    .then((data) => {
-      res.json(data);
-      console.log(data);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
+    res.json(workout);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
