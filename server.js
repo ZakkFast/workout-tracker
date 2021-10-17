67e457bb351fae460b9dc0002cc0d7bab81b571f
Zakk Fast <zakkfastpro@gmail.com>
2021-10-16 22:39:46 -0700

    cleaned up comments

--- server.js
+++ server.js
@@ -8,15 +8,14 @@ connectDB();
 //Init Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(logger("dev"))
-
-// app.get('/', (req, res) => res.send('API Running'));
+app.use(logger('dev'));
 
 app.use(express.static('public'));
-
 
 app.use(require('./routes/api'));
 app.use(require('./routes/index.js'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server started on port http://localhost:${PORT}`)
+);
